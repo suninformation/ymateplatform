72f1ed7c4132cc81564f93462652b5e042979f6e
suninformation <suninformation@163.com>
2014-12-26 19:04:29 +0800

    修正TreeObject转换JSON输出时，对KEY_CLASS赋值错误导致解析结果不一致的问题

--- ymateplatform/src/main/java/net/ymate/platform/commons/lang/TreeObject.java
+++ ymateplatform/src/main/java/net/ymate/platform/commons/lang/TreeObject.java
@@ -332,12 +332,12 @@ public class TreeObject implements Serializable, Cloneable {
                 // 保证数组的格式:{_value:[{_class:,_value},{}..]}
                 JSONObject _nodeJson = new JSONObject();
                 _nodeJson.put(KEY_VALUE, _itemJson);
-                _nodeJson.put(KEY_CLASS, MODE_ARRAY);
+                _nodeJson.put(KEY_CLASS, TYPE_COLLECTION);
                 return _nodeJson;
             } else {
                 JSONObject _nodeJson = new JSONObject();
                 _nodeJson.put(KEY_VALUE, new JSONArray());
-                _nodeJson.put(KEY_CLASS, MODE_ARRAY);
+                _nodeJson.put(KEY_CLASS, TYPE_COLLECTION);
                 return _nodeJson;
             }
         } else { // VALUE
@@ -2194,6 +2194,29 @@ public class TreeObject implements Serializable, Cloneable {
                 .put("detail", new TreeObject()
                         .put("real_name", "刘镇", true)
                         .put("age", 32));
+        //
+        // 这样赋值是List
+        TreeObject _list = new TreeObject();
+        _list.add("list item 1");
+        _list.add("list item 2");
+        // 这样赋值代表Map
+        TreeObject _map = new TreeObject();
+        _map.put("key1", "keyvalue1");
+        _map.put("key2", "keyvalue2");
+
+        TreeObject idsT = new TreeObject();
+        idsT.put("ids", _list);
+        idsT.put("maps", _map);
+
+        System.out.println(idsT.get("ids").isList());
+        System.out.println(idsT.get("ids").getList());
+        //
+        System.out.println(idsT.get("maps").isMap());
+        System.out.println(idsT.get("maps").getMap());
+        //
+        //
+        _target.put("map", _map);
+        _target.put("list", _list);
 
         String _jsonStr = _target.toJson().toJSONString();
         System.out.println(_jsonStr);
